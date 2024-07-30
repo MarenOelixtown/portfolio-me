d1608bd42b724b58892713fe536fdd2264c2e118
Maren <marenehlers@gmx.net>
2024-07-30 11:44:22 +0200

    adding image-portrait and emoji

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,5 +1,43 @@
+"use client";
+
+import Image from "next/image";
 import React from "react";
+import mePortrait from "../public/mePortrait.jpg";
+import { motion } from "framer-motion";
 
 export default function Intro() {
-  return <section></section>;
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src={mePortrait}
+              alt="Maren Portrait"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+
+          <motion.span
+            className="absolute bottom-0 left-0 text-3xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            ✌️
+          </motion.span>
+        </div>
+      </div>
+    </section>
+  );
 }
