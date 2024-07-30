e3f3061ff02f82cdd69fc5b3e9436f75dbc24990
Maren <marenehlers@gmx.net>
2024-07-30 13:30:10 +0200

    adding button and links

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,6 +4,10 @@ import Image from "next/image";
 import React from "react";
 import mePortrait from "../public/mePortrait.jpg";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -53,6 +57,42 @@ export default function Intro() {
         </span>{" "}
         finde ich, ist ein treffendes Motto.
       </motion.p>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/meCV.pdf"
+          download
+        >
+          Downlowd CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />{" "}
+        </a>
+        <a
+          className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/maren-ehlers-a262a4122/"
+          target="_blank"
+        >
+          <BsLinkedin />{" "}
+        </a>
+        <a
+          className="bg-white p-4 flex text-gray-700 items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15]  hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/MarenOelixtown"
+          target="_blank"
+        >
+          <FaGithubSquare />{" "}
+        </a>
+      </motion.div>
     </section>
   );
 }
