befd743eab118fe3a284a5ff7d7fac0de1a8206e
Maren <marenehlers@gmx.net>
2024-07-30 12:26:14 +0200

    adding intro-text

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -38,6 +38,21 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Moin, ich bin Maren.</span> Ich bin{" "}
+        <span className="font-bold">Junior Web Developer</span> mit über 15
+        Jahre Erfahrung als Bildredakteurin und von Natur aus aufgeschlossen und
+        neugierig.{" "}
+        <span className="italic">
+          „Neue Herausforderungen sind wunderbare Gelegenheiten, Neues über sich
+          selbst zu erfahren“,
+        </span>{" "}
+        finde ich, ist ein treffendes Motto.
+      </motion.p>
     </section>
   );
 }
