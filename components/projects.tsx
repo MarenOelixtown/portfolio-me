9838451199f4410e7ce5f8fd15467d5320f18cb1
Maren <marenehlers@gmx.net>
2024-08-06 13:10:43 +0200

    adding heading

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import SectionHeading from "./section-heading";
 
 export default function Projects() {
-  return <section></section>;
+  return (
+    <section>
+      <SectionHeading>My projects</SectionHeading>
+    </section>
+  );
 }
