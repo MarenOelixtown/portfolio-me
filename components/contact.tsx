9963d3ee7fcaeceab03d7e89375e36fa722b8a51
Maren <marenehlers@gmx.net>
2024-08-10 14:52:47 +0200

    refactoring submit-button and adding useFormStatus-hook

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { useSectionInView } from "../lib/hooks";
 import { sendEmail } from "../actions/sendEmail";
+import SubmitButton from "./submit-button";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -34,7 +34,6 @@ export default function Contact() {
           console.log("Running on client");
           console.log(formData.get("senderEmail"));
           console.log(formData.get("message"));
-          console.log(formData);
           await sendEmail(formData);
         }}
       >
@@ -53,13 +52,7 @@ export default function Contact() {
           maxLength={5000}
           className="h-52 my-3 rounded-lg borderBlack p-4"
         />
-        <button
-          type="submit"
-          className="group flex justify-center items-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
-        >
-          Senden{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
-        </button>
+        <SubmitButton />
       </form>
     </motion.section>
   );
